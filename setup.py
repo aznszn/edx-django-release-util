4f4531cb5b3d13a034798ac27cafd376bc7cc198
Azan Ali <azan.ali@arbisoft.com>
2026-03-25 11:38:50 +0500

    ran make upgrade and removed django 4.2 references

--- setup.py
+++ setup.py
@@ -145,7 +145,6 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Framework :: Django',
-        'Framework :: Django :: 4.2',
         'Framework :: Django :: 5.2',
     ],
 )
